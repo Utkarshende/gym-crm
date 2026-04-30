30e2a872d706b5308e1ee7465e6bc774263fb56e
Utkarshende <utkarshapd1403@gmail.com>
2026-04-30 23:59:39 +0530

    feat:created button.jsx reusable ui component

---
+++ client/src/components/ui/Button.jsx
@@ -0,0 +1,12 @@
+function Button({ children, onClick, className }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
