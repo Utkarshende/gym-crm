12cd2e99f876292671081bd91ca07be00b3c9b57
Utkarshende <utkarshapd1403@gmail.com>
2026-05-02 21:32:54 +0530

    feat: create member routes and controller

--- server/server.js
+++ server/server.js
@@ -1,30 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
 import memberRoutes from "./routes/memberRoutes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.use("/api/members", memberRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(
-    `Server running on ${PORT}`
-  );
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
