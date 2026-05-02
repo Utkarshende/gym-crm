72238f2ecaf3de018d269fefd11b8f795457aba1
Utkarshende <utkarshapd1403@gmail.com>
2026-05-02 16:30:28 +0530

    feat: create reusable edit member page using member form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,19 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AddMember from "./pages/AddMember";
+import EditMember from "./pages/EditMember";
+import ViewMode from "./pages/ViewMode";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/add-member"
-          element={<AddMember />}
-        />
-
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/add-member" element={<AddMember />} />
+        <Route path="/member/edit/:id" element={<EditMember />} />
+        <Route path="/member/view/:id" element={<ViewMode />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
