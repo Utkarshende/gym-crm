fb2c0e289a4a301a2c68bd1996621c6ee90a205e
Utkarshende <utkarshapd1403@gmail.com>
2026-05-03 23:36:14 +0530

    feat:added protected route in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,67 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import AddMember from "./pages/AddMember";
 import EditMember from "./pages/EditMember";
 import ViewMode from "./pages/ViewMode";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add-member" element={<AddMember />} />
-        <Route path="/member/edit/:id" element={<EditMember />} />
-        <Route path="/member/view/:id" element={<ViewMode />} />
+
+        {/* Default → Login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/add-member"
+          element={
+            <ProtectedRoute>
+              <AddMember />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/member/edit/:id"
+          element={
+            <ProtectedRoute>
+              <EditMember />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/member/view/:id"
+          element={
+            <ProtectedRoute>
+              <ViewMode />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
