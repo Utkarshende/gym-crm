2ea5602ff3aa50bf80de9185b38bcff7ba1ed41c
Utkarshende <utkarshapd1403@gmail.com>
2026-05-05 00:02:17 +0530

    fixed:token missing in api.js

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+
+export const GENDER_OPTIONS = ["Male", "Female", "Other"];
+
+export const STATUS_OPTIONS = [
+  { label: "Active", value: "active" },
+  { label: "On Break", value: "paused" },
+  { label: "Inactive", value: "expired" },
+];
+
+export const PLAN_OPTIONS = ["monthly", "quarterly"];
