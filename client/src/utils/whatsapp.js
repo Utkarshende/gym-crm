12db0dd8f02dd5f01225752d698c8849d4b2853c
Utkarshende <utkarshapd1403@gmail.com>
2026-05-14 23:07:58 +0530

    changed the whatsapp reminder  msg

--- client/src/utils/whatsapp.js
+++ client/src/utils/whatsapp.js
@@ -1,9 +1,8 @@
 export const sendFeeReminder = (member, currentMonth = "") =>{
     const phone = `91${member.phone}`;
 
-    const message =
-      `Hello ${member.name}, this is a reminder that your gym fee of ₹${member.fee} for ${currentMonth} is pending. Please pay soon. <br /> - SFC Gym`;
-
+const message = `Hello ${member.name}, this is a reminder that your gym fee of ₹${member.fee} for ${currentMonth} is pending. Please pay soon.
+\n*SFC Gym*`;
     const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
 };
