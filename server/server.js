61c16a7221f778ad41db52069d4f97eda1437c2a
Utkarshende <utkarshapd1403@gmail.com>
2026-05-14 22:30:17 +0530

    fixed render port issue

--- server/server.js
+++ server/server.js
@@ -34,6 +34,6 @@ app.use("/api/attendance", attendanceRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
