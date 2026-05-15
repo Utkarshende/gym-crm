706fecd57d2647c593d9be3f5b4d73973ee84f96
Utkarshende <utkarshapd1403@gmail.com>
2026-05-15 21:07:36 +0530

    fix: assigning user id in req.user

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -18,7 +18,11 @@ const authMiddleware = (req, res, next) => {
       process.env.JWT_SECRET
     );
 
-    req.user = decoded;
+    req.user ={ 
+  _id: decoded._id || decoded.id, 
+  id: decoded._id || decoded.id,
+  ...decoded 
+};
 
     next();
 
