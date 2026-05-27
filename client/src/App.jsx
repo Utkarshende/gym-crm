1d9522bdc7d6da7b7e6ac06136ce178b980005f0
Utkarshende <utkarshapd1403@gmail.com>
2026-05-27 20:20:07 +0530

    added aiworkout route in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import ViewMember from "./pages/ViewMember";
 import Payments from "./pages/Payments";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AIWorkout from "./pages/AIWorkout";
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
@@ -55,6 +56,10 @@ function App() {
 
         <Route path="*" element={<Navigate to="/dashboard" />} />
 
+<Route
+  path="/ai-workout"
+  element={<AIWorkout />}
+/>
 
       </Routes>
     </BrowserRouter>
